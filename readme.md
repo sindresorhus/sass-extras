52164721887e419414a0e645ff679d87ef9b9137
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-07 23:12:22 +0700

    Improve docs and add more tests

--- readme.md
+++ readme.md
@@ -11,6 +11,10 @@ It includes functions for using SVG inline, generating random colors, system fon
 $ npm install sass-extras
 ```
 
+<a href="https://www.patreon.com/sindresorhus">
+	<img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
 
 ## Usage
 
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import util from 'util';
-import test from 'ava';
+import {serial as test} from 'ava';
 import sass from 'node-sass';
 
 const render = data => {
@@ -29,6 +29,37 @@ test('@strip-unit()', t => {
 	snapshotFn(t, 'strip-unit(1)');
 });
 
+test('@clamp()', t => {
+	snapshotFn(t, 'clamp(4, 5, 10)');
+	snapshotFn(t, 'clamp(10px, 1px, 5px)');
+});
+
+test('@string-replace()', t => {
+	snapshotFn(t, 'string-replace(\'foo bar baz\', $search: \'bar\', $replacement: \'unicorn\')');
+	snapshotFn(t, 'string-replace(\'foo bar baz\', $search: \'non-existent\', $replacement: \'unicorn\')');
+});
+
 test('@url-encode()', t => {
 	snapshotFn(t, 'url-encode(\'#foo@bar\')');
+});
+
+test('@random-color()', t => {
+	t.regex(render('a { width: random-color() }'), /a { width: #[a-z\d]+; }/);
+	t.regex(render('a { width: random-color($saturation: 0.8, $lightness: 0.3) }'), /a { width: #[a-z\d]+; }/);
+});
+
+test('@tint()', t => {
+	snapshotFn(t, 'tint(pink, 10%)');
+});
+
+test('@shade()', t => {
+	snapshotFn(t, 'shade(pink, 10%)');
+});
+
+test('@svg-url()', t => {
+	snapshotFn(t, 'svg-url(\'<svg width="8" height="13"><defs><path d="M6.828 5.368c-"/></defs></svg>\')');
+});
+
+test('@background-svg()', t => {
+	snapshotProp(t, '@include background-svg(\'<svg width="8" height="13"><defs><path d="M6.828 5.368c-"/></defs></svg>\')');
 });
